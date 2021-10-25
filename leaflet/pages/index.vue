227a13d552c2dbe63bf456d64b5f81eacca81ce0
keerthana <keerthanaravichandran96@gmail.com>
2021-10-25 23:28:37 +0530

    leaflet view added

---
+++ leaflet/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map :zoom="13" :center="[11.892078, 79.827851]">
+        <l-tile-layer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { loc: [11.892078, 79.827851], title: 'pondy' },
+        { loc: [13.892078, 18.827851], title: 'chennai' },
+      ],
+    }
+  },
+}
+</script>
